e8160881ff4187441565b61a5324d9877bfc6628
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-07-25 14:17:00 -0300

    25/07/2022

---
+++ fitacrepe.js
@@ -0,0 +1,14 @@
+class Fitacrepe{
+    constructor(corpo1,corpo2){
+        var ultimabolacha=corpo1.body.bodies.length-2;
+        this.tecbond=Constraint.create({
+            bodyA:corpo1.body.bodies[ultimabolacha],
+            pointA:{x:0,y:0},
+            bodyB:corpo2,
+            pointB:{x:0,y:0},
+            length:-10,
+            stiffness:0.01
+        })
+        World.add(engine.world,this.tecbond);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -10,6 +10,18 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var piso;
+var teiadomiranha;
+var lemon;
+var colabastao;
+var papeldeparede;
+var magali;
+var sansao;
+
+function preload(){
+  papeldeparede = loadImage("./Imagens/background.png");
+  magali = loadImage("./Imagens/melon.png");
+  sansao = loadImage("./Imagens/Rabbit-01.png");
+}
 
 function setup() 
 {
@@ -19,16 +31,25 @@ function setup()
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  imageMode(CENTER);
   textSize(50);
 
   piso = new Piso(200,690,600,20);
+  teiadomiranha=new Rope(6,{x:245,y:30});
+  lemon=Bodies.circle(300,300,15);
+  Matter.Composite.add(teiadomiranha.body,lemon);
+  colabastao=new Fitacrepe(teiadomiranha,lemon);
 }
 
 function draw() 
 {
   background(51);
+  image(papeldeparede, width/2, height/2, 500, 700);
+
   Engine.update(engine);
   piso.tosemideia();
+  teiadomiranha.show();
+  image(magali,lemon.position.x,lemon.position.y,60,60);
 }
 
 
